17726c9a0a434b716c8d2526167704e200bc46e0
elhamzi-abderrahim <abderrahimelhamzi.pr@gmail.com>
2025-10-25 09:47:04 -0400

    remove rtl_project_t to add as submodule

--- rtl_project_t/do/waves.do
+++
@@ -1,2 +0,0 @@
-add wave -position insertpoint sim:/testbench/dut/clk
-add wave -position insertpoint sim:/testbench/dut/reset_n
--- rtl_project_t/tb/tb_defines.sv
+++
@@ -1,9 +0,0 @@
-`ifndef TB_DEFINES_SV
-`define TB_DEFINES_SV
-
-    `ifndef PARAM_VALUE
-        `define PARAM_VALUE 7
-    `endif
-
-
-`endif
